6658b69521c8598f87cfb9b5a26749a8af76c88e
allendii57-boop <allendii57@gmail.com>
2026-05-20 16:19:53 +1000

    Update quick_places_list.dart

--- lib/quick_places_list.dart
+++ lib/quick_places_list.dart
@@ -1,8 +1,11 @@
-// lib/quick_places_list.dart
-
 final List<Map<String, dynamic>> vanimoLocations = [
-   {"name": "Wesan Camp Market", "lat": -2.689952, "lng": 141.295191},
-    {"name": "Vanimo Airport", "lat": -2.688349, "lng": 141.299681},
-    {"name": "Top Rooster", "lat": -2.684457, "lng": 141.301108},
+{"name": "JB Fuel", "lat": -2.695689, "lng": 141.302769},
+{"name": "Papindo", "lat": -2.684999, "lng": 141.300611},
+{"name": "Tinglaching Enterprise Ltd", "lat": -2.695996, "lng": 141.303164},
+{"name": "Top Rooster", "lat": -2.684457, "lng": 141.301108},
+{"name": "Vanimo Airport", "lat": -2.688349, "lng": 141.299681},
+{"name": "Vanimo Supermarket Ltd", "lat": -2.685466, "lng": 141.300065},
+{"name": "Wesan Camp Market", "lat": -2.689952, "lng": 141.295191},
+{"name": "West Sepik Investment Ltd", "lat": -2.694577, "lng": 141.300342},
+{"name": "West Trading", "lat": -2.694525, "lng": 141.300261},
 {"name": "Yajada", "lat": -2.682954, "lng": 141.300161},
-];
