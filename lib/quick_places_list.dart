8511ac3996199f7c38f027c1d1f4176462b1878a
allendii57-boop <allendii57@gmail.com>
2026-05-20 16:46:19 +1000

    Update quick_places_list.dart

--- lib/quick_places_list.dart
+++ lib/quick_places_list.dart
@@ -1,4 +1,4 @@
-final List<Map<String, dynamic>> vanimoLocations = [
+⁶final List<Map<String, dynamic>> vanimoLocations = [
 {"name": "JB Fuel", "lat": -2.695689, "lng": 141.302769},
 {"name": "Papindo", "lat": -2.684999, "lng": 141.300611},
 {"name": "Tinglaching Enterprise Ltd", "lat": -2.695996, "lng": 141.303164},
@@ -9,3 +9,4 @@ final List<Map<String, dynamic>> vanimoLocations = [
 {"name": "West Sepik Investment Ltd", "lat": -2.694577, "lng": 141.300342},
 {"name": "West Trading", "lat": -2.694525, "lng": 141.300261},
 {"name": "Yajada", "lat": -2.682954, "lng": 141.300161},
+  ];
